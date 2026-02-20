c9b0c4d1ab8e0155fe4ab6a0c58824d247bc6841
Mohamed Osoble <mohamedosoble@outlook.com>
2026-02-20 22:58:48 +0000

    fix: add index in routes and fix router export in user

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes");
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = 3000;
+
+app.use("/user", routes.user);
 app.listen(PORT, (error) => {
   // This is important!
   // Without this, any startup errors will silently fail
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,25 +1,25 @@
-const { Router } = require('express');
-const controller = require('../controllers/user');
+const { Router } = require("express");
+const controller = require("../controllers/user");
 // const passport = require("passport");
 // const auth = passport.authenticate("jwt", { session: false });
 
 const router = Router();
 
 // Login route
-router.get('/login', (req, res) => {
-  return res.json('This is a post only route');
+router.get("/login", (req, res) => {
+  return res.json("This is a post only route");
 });
 
-router.post('/login', controller.login);
+router.post("/login", controller.login);
 
 // Logout route
-router.get('/logout', (req, res) => {
-  return res.json('This is a post only route');
+router.get("/logout", (req, res) => {
+  return res.json("This is a post only route");
 });
 
-router.post('/logout', controller.logout);
+router.post("/logout", controller.logout);
 
 // Is logged in route
-router.get('/isLoggedIn', controller.isLoggedIn);
+router.get("/isLoggedIn", controller.isLoggedIn);
 
-module.exports = { router };
+module.exports = router;
